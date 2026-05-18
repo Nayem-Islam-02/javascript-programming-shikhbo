2dd618e995648ff55c2d924728ebf6143ab97741
jarif hasan <hasanjarif939@gmail.com>
2026-05-18 16:42:41 +0600

    comment

--- vid2.js
+++ vid2.js
@@ -1,3 +1,3 @@
 // console ar moodhe js practice kora jay abar online editor use kora jay
 
-console.log(100+30)
+// console.log(100+30)
---
+++ vid3.js
@@ -0,0 +1,17 @@
+// chaile html ar vitore o js kora jay ,jemon:
+{/* <script> */}
+    {/* ar vitore js code lekha jay */}
+// </script>
+
+// console.log(20+39)
+
+// document.write('aita amr js ar third part');
+
+
+// alert('aikhane click korben nh')
+
+// alert(100+20)
+
+
+console.log('hello world');
+console.log(29+39)
