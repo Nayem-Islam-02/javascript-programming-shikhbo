2e44a9ef54c09b9d98efcdf046cf195ae9883483
jarif hasan <hasanjarif939@gmail.com>
2026-05-23 19:35:49 +0600

    comment

---
+++ vid4.js
@@ -0,0 +1,10 @@
+// 1.Number : 10 , 12 ,13, 14; 
+//  2. Integer:5 , 13 , 14;
+// 3.Float:Decimal:10.23 , 14.234, 24.45;
+//  4.-5 , 5  , 10 ,-13
+// 5.Operation:Addition+, Subtraction-, Multiplication *,Division /;
+// 6.Parcent %,
+// 7.Average;
+// 8.Comparison;
+// < ,> ,==, !=, ===;
+// 9.Remainder, Modulus 
