58bb69dc54bc60c78e37ad91bdb8115c60bae07e
jarif hasan <hasanjarif939@gmail.com>
2026-05-25 18:12:06 +0600

    comment

---
+++ vid10.js
@@ -0,0 +1,20 @@
+// 
+// 1.number 
+// 2.string
+// 3.boolean
+// 4.array
+
+// Array:
+var public1 = 50;
+var public2 = 100;
+var public3 = 500;
+var public4 = 1000;
+
+var publics = [50 , 100 , 500 , 1000];
+
+var friendsAge = [15 , 26, 18, 30, "hello"];
+
+console.log(friendsAge);
+
+var friendsName = ['Rubel', 'Mokku', 'sUMIT', 'NOBIN'];
+console.log(friendsName);
---
+++ vid9.js
@@ -0,0 +1,24 @@
+var hero = typeof " hero alam-22";
+
+var num = 22.4;
+var vatkhaiso = true;
+// console.log(8 > 10);
+// console.log(typeof vatkhaiso);
+// console.log( typeof num);
+// console.log(hero);
+
+var x = 'hero alam';
+
+x = 'shakib khan';
+console.log(x);
+x = "allu arjun"
+// js shob shomoy upor theke nicher dike run hoi
+
+
+var e = 10;
+e = 20;
+
+console.log(e);
+
+e = 40;
+console.log(e)
