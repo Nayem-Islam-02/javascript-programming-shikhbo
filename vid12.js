6655cc8bed5f3c7afe45ca4e463cee6cf68bf9b5
jarif hasan <hasanjarif939@gmail.com>
2026-05-31 06:46:44 +0600

    comment

---
+++ vid12.js
@@ -0,0 +1,31 @@
+// practice:
+
+var bondhuGon = [12, 13, 14, 15, 16, 17];
+
+var bondhuNam = ["Kala", "Shemol", "Jibon", "Ami", "Janam"]
+
+// console.log(bondhuGon);
+// console.log(bondhuNam);
+
+// console.log(typeof bondhuGon);
+// console.log(typeof bondhuNam);
+
+// console.log(bondhuGon.length);
+// console.log(bondhuNam.length);
+
+// console.log(bondhuGon[3]);
+
+// console.log(bondhuGon.indexOf(15));
+
+// var ashikB = [ 13, 14, 14, 24, 24, 244];
+// console.log(ashikB);
+
+// var notunN = ashikB.indexOf(13);
+// console.log(notunN);
+
+// var amiN = ashikB[3];
+// console.log(amiN);
+
+// -------------
+
+
