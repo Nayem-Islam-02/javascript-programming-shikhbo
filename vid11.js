2ec292be809435ca7d55e3dc60d4455a9a010251
jarif hasan <hasanjarif939@gmail.com>
2026-05-31 06:17:36 +0600

    comment

---
+++ vid11.js
@@ -0,0 +1,27 @@
+var friendsBoss = [15, 14, 23, 23, 24];
+var friendsNam = ["Rubel", "Mokku", "Jibon", "Kamon"];
+
+// console.log(typeof friendsBoss);
+// console.log(friendsBoss.length);
+// length ar maddhobe koita upadan ase jana jay
+
+// console.log(friendsNam.length);
+
+// console.log(friendsBoss.indexOf(23));
+// console.log(friendsBoss.indexOf(15));
+// console.log(friendsNam.indexOf__("Kamon"));
+
+// --------------
+
+// jekono position ke baireo ber korta pari:
+
+// var indexElement = friendsBoss[2];
+// console.log(indexElementj);
+
+console.log(friendsNam[3]);
+
+// learning outcomes:
+// 1.typeof 
+// 2.length
+// 3.indexOf
+// 4.variable moddhe theke index ar man ber kora
